a887080ac419c8e52bd3573b7bae5062872ef8c8
iMasanari <14dd023@gmail.com>
2018-07-12 21:46:59 +0900

    Updates

--- site-route.js
+++ site-route.js
@@ -9,25 +9,11 @@ var routes = [
 ];
 
 import { h } from 'hyperapp'
-import { Route } from '@hyperapp/router'
-import { __assign } from 'tslib'
 
 // declear var routes;
 
-export default (function () {
-  return (h(Route, {
-    render: function (props) {
-      return function (state) {
-        var Component = routes[state.data.component];
-
-        if (typeof document === 'object' && state.data.props) {
-          document.title = state.data.props.title;
-        }
-
-        return h(Component, __assign({}, (state.data.props), {
-          key: props.location.pathname
-        }))
-      };
-    }
-  }));
-});
+export default function () {
+  return function (state) {
+    return h(routes[state.data.component], state.data.props);
+  };
+}
