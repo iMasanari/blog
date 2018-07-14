b874aa9f29311d97ab658d8bc5f5d3f1993fdeeb
iMasanari <14dd023@gmail.com>
2018-07-14 11:12:21 +0900

    fix: markdown内の外部リンクがクリックできなかった問題を修正

--- src/containers/Post.tsx
+++ src/containers/Post.tsx
@@ -10,6 +10,17 @@ interface Props {
   next?: Pick<Post, 'title' | 'slug'>
 }
 
+const clickHandler = (e: Event) => {
+  if (e.target instanceof HTMLAnchorElement) {
+    const href = e.target.getAttribute('href')
+
+    if (!/^http/.test(href)) {
+      e.preventDefault()
+      history.pushState(location.pathname, '', href)
+    }
+  }
+}
+
 export default ({ post, prev, next }: Props) =>
   <div>
     <article>
@@ -18,17 +29,8 @@ export default ({ post, prev, next }: Props) =>
       <PostTags tags={post.tags} />
       <div
         innerHTML={post.contents}
-        oncreate={(el: Element) => {
-          el.innerHTML = post.contents
-          el.addEventListener('click', (e) => {
-            const target = e.target
-
-            if (target instanceof HTMLAnchorElement && !/$http/.test(target.href)) {
-              e.preventDefault()
-              history.pushState(location.pathname, '', target.href)
-            }
-          })
-        }}
+        oncreate={(el: Element) => { el.innerHTML = post.contents }}
+        onclick={clickHandler}
       />
     </article>
     <PostPager prev={prev} next={next} />
