aa98a4ce4dc96650e21e01ee9cea314ec98465a0
iMasanari <14dd023@gmail.com>
2018-08-10 22:20:17 +0900

    refactor(Template.tsx): 条件分岐を三項演算子からIfコンポネートへ変更など

--- src/Template.tsx
+++ src/Template.tsx
@@ -1,6 +1,6 @@
 import { h } from 'hyperapp'
 import App from './App'
-import { title, siteRoot, GA_TRACKING_ID } from './constants'
+import { title as siteTitle, siteRoot, GA_TRACKING_ID } from './constants'
 import { State } from '.';
 
 interface Props {
@@ -20,31 +20,53 @@ gtag('js', new Date());
 gtag('config', ${JSON.stringify(GA_TRACKING_ID)});
 `
 
-export default (props: Props) => (state: State) =>
-  <html lang="ja-JP" prefix="og: http://ogp.me/ns#">
-    <head>
-      {/* Global site tag (gtag.js) - Google Analytics */}
-      {process.env.NODE_ENV === 'production' ? <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} /> : null}
-      <script innerHTML={gtag} />
+const If = (props: { test: any }, children: any) =>
+  props.test ? children : null
 
-      {props.script ? <link rel="preload" href={props.script} as="script" /> : null}
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <title>{state.data.title || title}</title>
-      {props.css ? <link rel="stylesheet" href={props.css} /> : null}
+export default (props: Props) => (state: State) => {
+  const isProduction = process.env.NODE_ENV === 'production'
 
-      {/* OGPタグ */}
-      <meta property="og:title" content={state.data.title || title} />
-      <meta property="og:type" content={state.data.title ? 'article' : 'blog'} />
-      <meta property="og:description" content={props.meta.description || state.data.title || title} />
-      <meta property="og:url" content={siteRoot + state.location.pathname} />
-      <meta property="og:image" content={`${siteRoot}${props.meta.image || '/images/icon.jpg'}`} />
-      <meta name="twitter:card" content="summary" />
-    </head>
-    <body>
-      <App />
-      {state.data ? <script innerHTML={`\nvar __data = ${JSON.stringify(state.data, null, 2)};\n`} /> : null}
-      {props.script ? <script src={props.script} /> : null}
-    </body>
-  </html>
+  const title = state.data.title || siteTitle
+  const description = props.meta.description || title
+
+  return (
+    <html lang="ja-JP" prefix="og: http://ogp.me/ns#">
+      <head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <If test={isProduction}>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+        </If>
+        <script innerHTML={gtag} />
+
+        <If test={props.script}>
+          <link rel="preload" href={props.script} as="script" />
+        </If>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>{title}</title>
+        <If test={props.css}>
+          <link rel="stylesheet" href={props.css} />
+        </If>
+
+        {/* OGPタグ */}
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content={state.data.title ? 'article' : 'blog'} />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={siteRoot + state.location.pathname} />
+        <meta property="og:image" content={`${siteRoot}${props.meta.image || '/images/icon.jpg'}`} />
+        <meta name="twitter:card" content="summary" />
+      </head>
+      <body>
+        <App />
+
+        <If test={state.data}>
+          <script innerHTML={`\nvar __data = ${JSON.stringify(state.data, null, 2)};\n`} />
+        </If>
+        <If test={props.script}>
+          <script src={props.script} />
+        </If>
+      </body>
+    </html>
+  )
+}
