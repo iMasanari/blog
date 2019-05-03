44e95fcb292161d0602a87f309d8d28d86382097
iMasanari <14dd023@gmail.com>
2019-05-03 13:44:30 +0900

    refactor: アクション

--- src/index.ts
+++ src/index.ts
@@ -18,17 +18,17 @@ const state = { data }
 let currentPath: string
 
 const actions = {
-  setData(data: Data) {
+  setData: (data: Data) => {
     return { data }
   },
-  replace(path: string) {
+  replace: (path: string) => (state: State, actions: Actions) => {
     currentPath = path
 
     load(path, (data) => {
       if (path !== currentPath) return
 
       document.title = data.title
-      main.setData(data)
+      actions.setData(data)
 
       if (history.state && history.state.scrollTop) {
         // 描画後、スクロール位置を復元する
@@ -42,7 +42,7 @@ const actions = {
       gtag('config', GA_TRACKING_ID, { page_path: path })
     }
   },
-  linkHandler(event: MouseEvent) {
+  linkHandler: (event: MouseEvent) => (state: State, actions: Actions) => {
     const target = (event.currentTarget as HTMLElement).tagName.toLowerCase() === 'a'
       ? event.currentTarget
       : event.target
@@ -56,7 +56,7 @@ const actions = {
 
     history.replaceState({ scrollTop }, '', location.pathname)
     history.pushState({ scrollTop: 0 }, '', to)
-    main.replace(to)
+    actions.replace(to)
 
     setTimeout(() => scrollTo(0, 0))
   },
