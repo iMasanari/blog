1b6499543db04e2b1a84131fa305a28d6c2d079d
iMasanari <14dd023@gmail.com>
2021-09-12 11:53:06 +0900

    ページングの不正リンクを修正

--- src/components/molecules/Pager.tsx
+++ src/components/molecules/Pager.tsx
@@ -1,14 +1,14 @@
 import { makeStyles } from '@material-ui/core'
-import { Pagination, PaginationItem } from '@material-ui/lab'
+import { Pagination, PaginationItem, PaginationRenderItemParams } from '@material-ui/lab'
 import React from 'react'
 import LinkBehavior from '../atoms/LinkBehavior'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   root: {
     display: 'flex',
     justifyContent: 'center',
   },
-}))
+})
 
 interface Props {
   page: number
@@ -19,18 +19,26 @@ interface Props {
 export default function Pager({ page, count, basePath }: Props) {
   const classes = useStyles()
 
+  const renderItem = (item: PaginationRenderItemParams) => {
+    if (item.page <= 0 || count < item.page) {
+      return <PaginationItem component="span" {...item} />
+    }
+
+    return (
+      <PaginationItem
+        component={LinkBehavior}
+        href={`${basePath}${item.page === 1 ? '' : `p${item.page}`}`}
+        {...item}
+      />
+    )
+  }
+
   return (
     <Pagination
       className={classes.root}
       page={page}
       count={count}
-      renderItem={(item) =>
-        <PaginationItem
-          component={LinkBehavior}
-          href={`${basePath}${item.page === 1 ? '' : `p${item.page}`}`}
-          {...item}
-        />
-      }
+      renderItem={renderItem}
     />
   )
 }
