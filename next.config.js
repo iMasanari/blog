5b773224db0c8ac0bc73fdc16562d68273d12cce
iMasanari <14dd023@gmail.com>
2021-09-28 01:26:13 +0900

    ie11対応

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
 module.exports = {
   trailingSlash: true,
-  webpack(config, options) {
+  webpack(/** @type {import('webpack').Configuration} */ config, options) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@mui/core': '@mui/core/legacy',
+      '@mui/material': '@mui/material/legacy',
+      '@mui/private-theming': '@mui/private-theming/legacy',
+      '@mui/styled-engine': '@mui/styled-engine/legacy',
+      '@mui/system': '@mui/system/legacy',
+      '@mui/utils': '@mui/utils/legacy',
+    }
+
     config.module.rules.push({
       test: /\.mdx?$/i,
       use: 'raw-loader',
