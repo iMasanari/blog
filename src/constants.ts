abb375816909666172b3cb4ae0f40d224097bf9c
iMasanari <14dd023@gmail.com>
2023-01-22 13:22:57 +0900

    独自ドメインに変更

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SITE_NAME = 'Tech SANDBOX'
-export const SITE_ROOT = 'https://imasanari.github.io'
+export const SITE_ROOT = 'https://blog.imasanari.dev'
 
 export const POST_LIMIT_OF_PAGES = 5
 export const SUGGEST_LIMIT = 3
