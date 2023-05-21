da56b5d9b53dec5cebe9178ccbd18ea6fb0afaa5
iMasanari <14dd023@gmail.com>
2023-05-22 00:38:52 +0900

    マークダウン用のコンポーネントの追加

--- src/features/Markdown/index.ts
+++ src/features/Markdown/index.ts
@@ -1,11 +1,14 @@
 import Link from '../../components/Link.astro'
+import Blockquote from './components/Blockquote.astro'
 import Code from './components/Code.astro'
 import CodeWrapper from './components/CodeWrapper.astro'
 import H2 from './components/H2.astro'
 import H3 from './components/H3.astro'
 import H4 from './components/H4.astro'
+import Ol from './components/Ol.astro'
 import P from './components/P.astro'
 import Pre from './components/Pre.astro'
+import Ul from './components/Ul.astro'
 
 export const components = {
   a: Link,
@@ -15,5 +18,8 @@ export const components = {
   h3: H3,
   h4: H4,
   p: P,
+  blockquote: Blockquote,
+  ul: Ul,
+  ol: Ol,
   'app-code-wrapper': CodeWrapper,
 }
