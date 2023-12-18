0ba18731d61a71beca91763cbba7b460823ea1ae
iMasanari <14dd023@gmail.com>
2023-12-18 10:38:53 +0900

    404ページを追加

---
+++ src/features/NotFoundPage/NotFoundPage.styles.ts
@@ -0,0 +1,13 @@
+import { css } from 'molcss'
+
+export const notFound = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 4rem 1.25rem;
+`
+
+export const title = css`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+`
